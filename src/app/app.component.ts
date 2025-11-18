ccf0de5989152c4ec30f0156940068f2cf3984b6
lourdesperezhuaman <lourdesperezhuaman18@gmail.com>
2025-11-18 08:58:01 -0500

    configuracion de Firebase y AngularFire

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Firestore, collection, getDocs } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'restaurante-pedidos';
+  private firestore: Firestore = inject(Firestore);
+
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyC86l9F9h8tQ0hlnzvL7UY_8_oaeDJnQJA",
+    authDomain: "restaurante-pedidos-878fc.firebaseapp.com",
+    projectId: "restaurante-pedidos-878fc",
+    storageBucket: "restaurante-pedidos-878fc.firebasestorage.app",
+    messagingSenderId: "569417203324",
+    appId: "1:569417203324:web:26c8e6058746a2f84913f9"
+  }
+};
